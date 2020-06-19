6b0286c6b6f293c15b4ae02a92a7cf9ed3e5b5d8
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-06-19 12:38:19 +0300

    add presentational component

---
+++ src/Product.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import { IProduct } from './ProductsData';
+
+interface IProps {
+  product: IProduct;
+  inBasket: boolean;
+  onAddToBasket: () => void;
+}
+
+const Product: React.SFC<IProps> = (props) => {
+  const product = props.product;
+
+  const handleAddClick = () => {
+    props.onAddToBasket();
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p className="product-price">
+        {new Intl.NumberFormat('en-US', {
+          currency: 'USD',
+          style: 'currency',
+        }).format(product.price)}
+      </p>
+      {!props.inBasket && (
+        <button onClick={handleAddClick}>Add to basket</button>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Product;
