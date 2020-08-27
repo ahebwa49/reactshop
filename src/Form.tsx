674759e857c023bb059a135a4c9c7aa385155525
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-08-27 11:24:09 +0300

    add generic form components

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 
+interface IFormContext {
+  values: IValues;
+  setValue?: (fieldName: string, value: any) => void;
+}
+
+const FormContext = React.createContext<IFormContext>({
+  values: {},
+});
+
 export interface IValues {
   [key: string]: any;
 }
@@ -12,6 +21,13 @@ interface IState {
   values: IValues;
 }
 
+interface IFieldProps {
+  name: string;
+  label: string;
+  type?: 'Text' | 'Email' | 'Select' | 'TextArea';
+  options?: string[];
+}
+
 export class Form extends React.Component<IFormProps, IState> {
   constructor(props: IFormProps) {
     super(props);
@@ -19,11 +35,80 @@ export class Form extends React.Component<IFormProps, IState> {
       values: props.defaultValues,
     };
   }
-  public render() {
+
+  public static Field: React.SFC<IFieldProps> = (props) => {
+    const { name, label, type, options } = props;
+
+    const handleChange = (
+      e:
+        | React.ChangeEvent<HTMLInputElement>
+        | React.ChangeEvent<HTMLTextAreaElement>
+        | React.ChangeEvent<HTMLSelectElement>,
+      context: IFormContext
+    ) => {
+      if (context.setValue) {
+        context.setValue(props.name, e.currentTarget.value);
+      }
+    };
     return (
-      <form className="form" noValidate={true}>
-        {this.props.children}
-      </form>
+      <FormContext.Consumer>
+        {(context) => (
+          <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            {(type === 'Text' || type === 'Email') && (
+              <input
+                type={type.toLowerCase()}
+                id={name}
+                value={context.values[name]}
+                onChange={(e) => handleChange(e, context)}
+              />
+            )}
+            {type === 'TextArea' && (
+              <textarea
+                id={name}
+                value={context.values[name]}
+                onChange={(e) => handleChange(e, context)}
+              />
+            )}
+            {type === 'Select' && (
+              <select
+                value={context.values[name]}
+                onChange={(e) => handleChange(e, context)}
+              >
+                {options &&
+                  options.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+              </select>
+            )}
+          </div>
+        )}
+      </FormContext.Consumer>
+    );
+  };
+
+  private setValue = (fieldName: string, value: any) => {
+    const newValues = { ...this.state.values, [fieldName]: value };
+    this.setState({ values: newValues });
+  };
+
+  public render() {
+    const context: IFormContext = {
+      values: this.state.values,
+      setValue: this.setValue,
+    };
+    return (
+      <FormContext.Provider value={context}>
+        <form className="form" noValidate={true}>
+          {this.props.children}
+        </form>
+      </FormContext.Provider>
     );
   }
 }
+
+Form.Field.defaultProps = {
+  type: 'Text',
+};
